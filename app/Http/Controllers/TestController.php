d0f7235a3566c886174ebd5d05d0ec0b95730237
9breakers <9breakers@users.noreply.github.com>
2024-12-16 12:50:50 +0000

    Fixes coding style

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Contracts\CommentContracts\CommentSortIndexContract;
-use App\Contracts\CommentContracts\CommentTagHTMLContract;
-
-
-class TestController extends Controller{
-
+class TestController extends Controller
+{
     public function __call($method, $parameters)
     {
         parent::__call($method, $parameters);
