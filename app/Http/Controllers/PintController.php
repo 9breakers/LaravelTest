78471fa6b96af1e279887b26c9c766cebaea8a6f
9breakers <9breakers@users.noreply.github.com>
2024-12-16 12:59:22 +0000

    Fixes coding style

--- app/Http/Controllers/PintController.php
+++ app/Http/Controllers/PintController.php
@@ -2,30 +2,27 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Http\Request;
-use App\Models\User;  use App\Contracts\CommentContracts\CommentSortIndexContract;
-
-
+use App\Contracts\CommentContracts\CommentSortIndexContract;
 
 class PintController extends Controller
 {
-
     public function __construct(CommentSortIndexContract $CommentSortIndexContract)
-        {
-            $this->CommentSortIndexContract = $CommentSortIndexContract;
-        }
+    {
+        $this->CommentSortIndexContract = $CommentSortIndexContract;
+    }
 
-    public function index() {
+    public function index()
+    {
 
-        $data = array(
+        $data = [
             'user1' => 'John Doe',
-            'user2' => 'Jane Doe'
-        );
+            'user2' => 'Jane Doe',
+        ];
 
         $posts = [
             'post1' => 'Post 1',
             'post2' => 'Post 2',
         ];
-        return view('test.index', compact('data', 'posts')); }
+        return view('test.index', compact('data', 'posts'));
+    }
 }
