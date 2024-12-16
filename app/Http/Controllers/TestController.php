a35600d4bb34fff3746b49359edcc5d412fb8815
9breakers <9breakers@users.noreply.github.com>
2024-12-16 13:08:22 +0000

    Fixes coding style

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,30 +2,29 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Http\Request;
-use App\Models\User; use App\Contracts\CommentContracts\CommentSortIndexContract;
+use App\Contracts\CommentContracts\CommentSortIndexContract;
 
 class TestController extends Controller
 {
+    protected CommentSortIndexContract $CommentSortIndexContract;
 
-        protected CommentSortIndexContract $CommentSortIndexContract;
+    public function __construct(CommentSortIndexContract $CommentSortIndexContract)
+    {
+        $this->CommentSortIndexContract = $CommentSortIndexContract;
+    }
 
-        public function __construct(CommentSortIndexContract $CommentSortIndexContract)
-        {
-            $this->CommentSortIndexContract = $CommentSortIndexContract;
-        }
+    public function index()
+    {
 
-    public function index() {
-
-        $data = array(
+        $data = [
             'user1' => 'John Doe',
-            'user2' => 'Jane Doe'
-        );
+            'user2' => 'Jane Doe',
+        ];
 
         $posts = [
             'post1' => 'Post 1',
             'post2' => 'Post 2',
         ];
-        return view('test.index', compact('data', 'posts')); }
+        return view('test.index', compact('data', 'posts'));
+    }
 }
